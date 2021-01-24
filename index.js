65443f7d9821c63f9d521fcd78a277b0435e5903
reevesalic <reevesalic@gmail.com>
2021-01-24 12:23:24 -0500

    commit tree 83b3e92da4e9d12fe058cd8e4e1cfff2b3cc21e8

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+let random = Math.floor(Math.random() > 0);
+const mod = 16 % 6;
+const max = Math.max(10, 14, 20)
